dbcb3acc5ffff291604c515de134928b4d6d59cc
Geoffrey Martin <geoffm39@gmail.com>
2024-07-04 08:21:02 +1000

    setup app files

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
