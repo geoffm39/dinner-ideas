9f41237fde7116fbc77f41f4e3bb542bb2f3d416
Geoffrey Martin <geoffm39@gmail.com>
2024-07-09 18:08:56 +1000

    add form with css to search route

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+const cuisines = ["Any", "African", "Asian", "American", "British", "Cajun", "Caribbean", "Chinese", "European", 
+    "Eastern European", "French", "German", "Greek", "Indian", "Irish", "Italian", "Japanese", "Jewish", 
+    "Korean", "Latin American", "Mediterranean", "Mexican", "Middle Eastern", "Nordic", "Southern", 
+    "Spanish", "Thai", "Vietnamese"];
+
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
@@ -12,7 +17,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/search", (req, res) => {
-    res.render("search.ejs", { currentPage: "search" });
+    res.render("search.ejs", { currentPage: "search", cuisines: cuisines });
+});
+
+app.get("/results", (req, res) => {
+    const { query: { query, cuisine, includeIngredients }} = req;
+    res.render("results.ejs", { currentPage: "results"});
 });
 
 app.listen(port, () => {
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 $("#explore-button, #search-icon").click(function() {
     $(".content").fadeOut(500, function() {
-        window.location.href = "/search";    
+        window.location.href = "/search";
     });
 });
 
 $("#home-icon").click(function() {
     $(".content").fadeOut(500, function() {
-        window.location.href = "/";    
+        window.location.href = "/";        
     });
 });
